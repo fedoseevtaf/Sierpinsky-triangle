26f60b30199a870345f0a4f12222d080004720af
fedoseevtaf <76451152+fedoseevtaf@users.noreply.github.com>
2021-01-17 01:05:26 +0300

    first version

--- sfractal.py
+++ sfractal.py
@@ -1,5 +1,6 @@
 #==============================================================#
-iterCount = 1000
+iterCount = 3000
+width = 4
 
 #==============================================================#
 pos1 = 0, 200
@@ -7,7 +8,7 @@ pos2 = -200, -200
 pos3 = 200, -200
 
 #==============================================================#
-from random import random
+from random import randint as random_int
 import turtle as tp
 
 #==============================================================#
@@ -17,17 +18,61 @@ tp.speed(0)
 
 #==============================================================#
 
+
 class fractal_painter():
-    def __init__():
-        pass
+    def __init__(self):
+        self.pos = [0, 0]
 
 
-    def newpos():
-        pass
+    def newpos(self):
+        flag = random_int(1, 3)
+
+        if flag == 1:
+            self.pos[0] += pos1[0]
+            self.pos[0] /= 2
+
+            self.pos[1] += pos1[1]
+            self.pos[1] /= 2
+
+        elif flag == 2:
+            self.pos[0] += pos2[0]
+            self.pos[0] /= 2
+
+            self.pos[1] += pos2[1]
+            self.pos[1] /= 2
 
 
-    def dot():
-        pass
+        elif flag == 3:
+            self.pos[0] += pos3[0]
+            self.pos[0] /= 2
 
-    def _2paint():
-        pass
+            self.pos[1] += pos3[1]
+            self.pos[1] /= 2
+
+        else:
+            raise ValueError('Module \"random\" does not work correctly!')
+
+
+    def dot(self):
+        tp.pendown()
+        tp.width(width)
+        tp.forward(1)
+        tp.backward(1)
+        tp.penup()
+
+
+    def _2paint(self):
+        count = iterCount
+        
+        while count > 0:
+            tp.setx(self.pos[0])
+            tp.sety(self.pos[1])
+            
+            self.newpos()
+            self.dot()
+            
+            count -= 1
+
+
+myfractal = fractal_painter()
+myfractal._2paint()
